a5c3413f85484ca575bd624511341cf68fb9fc58
gbodeen <gabebodeen@gmail.com>
2018-11-05 15:14:16 -0600

    (Update) Add ES6 notation to getElementsByClassName function.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,6 +3,9 @@
 //   return document.getElementsByClassName(className);
 // };
 
+
+
+
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className, node = document.body, results = []) {
   
@@ -13,6 +16,19 @@ var getElementsByClassName = function(className, node = document.body, results =
   for (let child of node.childNodes) {
     getElementsByClassName(className, child, results);
   }
-  
+
   return results;
 };
+
+// const getElementsByClassName = (className, node = document.body, matchList = []) => { 
+//   // Check if node contains className
+//   if (node.className && node.className.split(' ').includes(className)) { 
+//     matchList.push(node); 
+//   }
+
+//   for (let child of node.childNodes) { 
+//     getElementsByClassName(className, child, matchList); 
+//   }
+  
+//   return matchList;
+// }
