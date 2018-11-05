265e010e4b2ad3d22a5e65ac44cbf1bd2dd0fc19
gbodeen <gabebodeen@gmail.com>
2018-11-05 16:08:38 -0600

    (feat) Add stringifyJSON function with higher order functions implementation. Update spec for stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,24 @@
 
 // but you don't so you're going to write it from scratch:
 
+// 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Check primitives 
+  if (typeof obj === 'string') { 
+    return '"' + obj + '"'; 
+  } else if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+    return '' + obj; 
+  } else if (Array.isArray(obj)) { // Check if input is an array 
+    return '[' + obj.map(stringifyJSON).join(',') + ']'; // ... and stringify all the elements
+  } else if (typeof obj === 'object') { 
+    return '{' + Object.keys(obj).filter( key => {
+      // JSON does not allow you to stringify functions or
+      // undefined values, so you should skip those key/value pairs
+      return obj[key] !== undefined && typeof obj[key] !== 'function';
+    })
+    .map( key => { 
+      // stringify all the key:value pairs
+      return stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+    }).join(',') + '}';
+  }
 };
