723e409ad0db69dd18a2b8a759cbf3ae2568e613
gbodeen <gabebodeen@gmail.com>
2018-11-05 20:05:37 -0600

    (feat?) Attempt parseJSON, it doesn't work yet

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,16 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // check first for the easy cases: booleans, null
+  if (json === 'true') {
+    return true;
+  } else if (json === 'false') {
+    return false;
+  } else if (json === 'null') {
+    return null;
+  } else if (!isNaN(Number(json))) {
+    return Number(json);
+  } else if (/^"([^"]|(\\"))*"$/.test(json)) { // broke
+    return json.slice(1, -1);
+  }
 };
